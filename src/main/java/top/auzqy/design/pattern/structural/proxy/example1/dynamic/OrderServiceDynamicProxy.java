1b37ea8b55f099790084652623234dcb2b1379a1
au <auzhouqingyu@163.com>
2019-07-21 23:51:13 +0800

    proxy example1 dynamic proxy 10%

---
+++ src/main/java/top/auzqy/design/pattern/structural/proxy/example1/dynamic/OrderServiceDynamicProxy.java
@@ -0,0 +1,9 @@
+package top.auzqy.design.pattern.structural.proxy.example1.dynamic;
+
+/**
+ * @Description: 
+ * @Author: zqy
+ * @CreateTime: 2019-07-21 23:18
+ */
+public class OrderServiceDynamicProxy {
+}
