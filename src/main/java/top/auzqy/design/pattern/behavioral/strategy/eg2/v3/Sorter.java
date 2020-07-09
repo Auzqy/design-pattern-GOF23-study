2e259cca87b170478109a68e958fba00e5c4096a
au <auzhouqingyu@163.com>
2020-07-09 20:06:16 +0800

    strategy eg2.v3

--- src/main/java/top/auzqy/design/pattern/behavioral/strategy/eg2/v3/Sorter.java
+++ src/main/java/top/auzqy/design/pattern/behavioral/strategy/eg2/v3/Sorter.java
@@ -4,6 +4,13 @@ import top.auzqy.design.pattern.behavioral.strategy.eg2.v2.ISortAlg;
 
 import java.io.File;
 
+/**
+ * description:  每种排序类都是无状态的，我们没必要在每次使用的时候，都重新创建一个新的对象。
+ *       所以，我们可以使用工厂模式对对象的创建进行封装。
+ *       我们通过策略模式将策略的定义、创建、使用解耦，让每一部分都不至于太复杂
+ * createTime: 2020/7/9 20:06
+ * @author au
+ */
 public class Sorter {
   private static final long GB = 1000 * 1000 * 1000;
 
