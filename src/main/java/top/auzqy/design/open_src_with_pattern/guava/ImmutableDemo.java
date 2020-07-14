f4374d030b4f079dd30ebf7f9e7d80b03c7e2d35
au <auzhouqingyu@163.com>
2020-07-14 15:35:43 +0800

    ImmutableDemo

---
+++ src/main/java/top/auzqy/design/open_src_with_pattern/guava/ImmutableDemo.java
@@ -0,0 +1,35 @@
+
+package top.auzqy.design.open_src_with_pattern.guava;
+
+import com.google.common.collect.ImmutableList;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ImmutableDemo {
+  public static void main(String[] args) {
+    List<String> originalList = new ArrayList<>();
+    originalList.add("a");
+    originalList.add("b");
+    originalList.add("c");
+
+    List<String> jdkUnmodifiableList = Collections.unmodifiableList(originalList);
+    List<String> guavaImmutableList = ImmutableList.copyOf(originalList);
+
+    //jdkUnmodifiableList.add("d"); // 抛出UnsupportedOperationException
+    // guavaImmutableList.add("d"); // 抛出UnsupportedOperationException
+    originalList.add("d");
+
+    print(originalList); // a b c d
+    print(jdkUnmodifiableList); // a b c d
+    print(guavaImmutableList); // a b c
+  }
+
+  private static void print(List<String> list) {
+    for (String s : list) {
+      System.out.print(s + " ");
+    }
+    System.out.println();
+  }
+}
