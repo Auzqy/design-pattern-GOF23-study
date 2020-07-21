30acad8a3a3b96589d739ee08917fc154da50614
au <auzhouqingyu@163.com>
2020-07-21 22:39:04 +0800

    project01 todo

---
+++ src/main/java/top/auzqy/design/actual_project_apply_with_pattern/project01/InternalErrorException.java
@@ -0,0 +1,11 @@
+package top.auzqy.design.actual_project_apply_with_pattern.project01;
+
+public class InternalErrorException extends Throwable {
+    public InternalErrorException(String s, InterruptedException e) {
+
+    }
+
+    public InternalErrorException(String s) {
+
+    }
+}
---
+++ src/main/java/top/auzqy/design/actual_project_apply_with_pattern/project01/Yaml.java
@@ -0,0 +1,10 @@
+package top.auzqy.design.actual_project_apply_with_pattern.project01;
+
+import java.io.InputStream;
+
+public class Yaml {
+    public RuleConfig loadAs(InputStream in, Class<RuleConfig> ruleConfigClass) {
+
+        return null;
+    }
+}
