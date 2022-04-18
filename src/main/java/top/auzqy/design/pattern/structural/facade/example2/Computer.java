75875eb475356070a049a04a9f35475435c28a47
au <au@MBP.com>
2022-04-18 11:01:05 +0800

    refact facade/example2/Computer

--- src/main/java/top/auzqy/design/pattern/structural/facade/example2/Computer.java
+++ src/main/java/top/auzqy/design/pattern/structural/facade/example2/Computer.java
@@ -3,33 +3,34 @@ package top.auzqy.design.pattern.structural.facade.example2;
 /**
  * description:  门面类： 计算机
  * createTime: 2019-08-03 11:06
+ *
  * @author zqy
  */
 public class Computer {
-	private CPU cpu;
-	private Memory memory;
-	private Disk disk;
+    private final CPU cpu;
+    private final  Memory memory;
+    private final  Disk disk;
 
-	public Computer() {
-		cpu = new CPU();
-		memory = new Memory();
-		disk = new Disk();
-	}
+    public Computer() {
+        cpu = new CPU();
+        memory = new Memory();
+        disk = new Disk();
+    }
 
-	public void startup() {
-		System.out.println("start the computer!");
-		cpu.startup();
-		memory.startup();
-		disk.startup();
-		System.out.println("start computer finished!");
-	}
+    public void startup() {
+        System.out.println("start the computer!");
+        cpu.startup();
+        memory.startup();
+        disk.startup();
+        System.out.println("start computer finished!");
+    }
 
-	public void shutdown() {
-		System.out.println("begin to close the computer!");
-		cpu.shutdown();
-		memory.shutdown();
-		disk.shutdown();
-		System.out.println("computer closed!");
-	}
+    public void shutdown() {
+        System.out.println("begin to close the computer!");
+        cpu.shutdown();
+        memory.shutdown();
+        disk.shutdown();
+        System.out.println("computer closed!");
+    }
 
 }
