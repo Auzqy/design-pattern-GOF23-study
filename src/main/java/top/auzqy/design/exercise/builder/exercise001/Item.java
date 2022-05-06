068efcf8308276071fce20e045a5da44773054bf
au-win <auzhouqingyu@163.com>
2022-05-06 17:06:05 +0800

    builder exercise001 create interface

---
+++ src/main/java/top/auzqy/design/exercise/builder/exercise001/Item.java
@@ -0,0 +1,7 @@
+package top.auzqy.design.exercise.builder.exercise001;
+
+public interface Item {
+   public String name();
+   public Packing packing();
+   public float price();    
+}
---
+++ src/main/java/top/auzqy/design/exercise/builder/exercise001/Packing.java
@@ -0,0 +1,5 @@
+package top.auzqy.design.exercise.builder.exercise001;
+
+public interface Packing {
+   public String pack();
+}
