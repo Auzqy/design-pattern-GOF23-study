c2e862b6f625bd8425da780b7a13b1162e416ce8
au-win <auzhouqingyu@163.com>
2022-05-13 11:39:24 +0800

    structural adapter example5 MediaPlayer and AdvanceMediaPlayer

---
+++ src/main/java/top/auzqy/design/pattern/structural/adapter/example5/AdvancedMediaPlayer.java
@@ -0,0 +1,6 @@
+package top.auzqy.design.pattern.structural.adapter.example5;
+
+public interface AdvancedMediaPlayer {
+   public void playVlc(String fileName);
+   public void playMp4(String fileName);
+}
---
+++ src/main/java/top/auzqy/design/pattern/structural/adapter/example5/MediaPlayer.java
@@ -0,0 +1,5 @@
+package top.auzqy.design.pattern.structural.adapter.example5;
+
+public interface MediaPlayer {
+   public void play(String audioType, String fileName);
+}
