5b88abdff6c16c79c5cf7300c6bf3400709015f5
au-win <auzhouqingyu@163.com>
2022-05-16 08:35:34 +0800

    bridge example001 bridgePatternDemo

---
+++ src/main/java/top/auzqy/design/pattern/structural/bridge/example001/Shape.java
@@ -0,0 +1,9 @@
+package top.auzqy.design.pattern.structural.bridge.example001;
+
+public abstract class Shape {
+   protected DrawAPI drawAPI;
+   protected Shape(DrawAPI drawAPI){
+      this.drawAPI = drawAPI;
+   }
+   public abstract void draw();  
+}
