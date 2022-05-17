6b4026749650a990ccdcb26c87ab92e8802c4f50
au-win <auzhouqingyu@163.com>
2022-05-17 16:02:45 +0800

    structural decorator example3 Circle and Rectangle and Shape

---
+++ src/main/java/top/auzqy/design/pattern/structural/decorator/example3/Circle.java
@@ -0,0 +1,9 @@
+package top.auzqy.design.pattern.structural.decorator.example3;
+
+public class Circle implements Shape {
+ 
+   @Override
+   public void draw() {
+      System.out.println("Shape: Circle");
+   }
+}
