16b5872c2042c3fd9d2e3d5d1e955d41adff06ab
au-win <auzhouqingyu@163.com>
2022-05-18 14:27:08 +0800

    structional facade example3 ShapeMaker and FacadePatternDemo

---
+++ src/main/java/top/auzqy/design/pattern/structural/facade/example3/FacadePatternDemo.java
@@ -0,0 +1,11 @@
+package top.auzqy.design.pattern.structural.facade.example3;
+
+public class FacadePatternDemo {
+   public static void main(String[] args) {
+      ShapeMaker shapeMaker = new ShapeMaker();
+ 
+      shapeMaker.drawCircle();
+      shapeMaker.drawRectangle();
+      shapeMaker.drawSquare();      
+   }
+}
