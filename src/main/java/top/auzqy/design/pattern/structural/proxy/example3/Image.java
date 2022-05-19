1681aad2bdbe47cc800187f5d4afc7031f5d2368
au-win <auzhouqingyu@163.com>
2022-05-19 17:27:03 +0800

    structural proxy example3 Image and ProxyImage and RealImage

---
+++ src/main/java/top/auzqy/design/pattern/structural/proxy/example3/Image.java
@@ -0,0 +1,5 @@
+package top.auzqy.design.pattern.structural.proxy.example3;
+
+public interface Image {
+   void display();
+}
---
+++ src/main/java/top/auzqy/design/pattern/structural/proxy/example3/RealImage.java
@@ -0,0 +1,20 @@
+package top.auzqy.design.pattern.structural.proxy.example3;
+
+public class RealImage implements Image {
+ 
+   private String fileName;
+ 
+   public RealImage(String fileName){
+      this.fileName = fileName;
+      loadFromDisk(fileName);
+   }
+ 
+   @Override
+   public void display() {
+      System.out.println("Displaying " + fileName);
+   }
+ 
+   private void loadFromDisk(String fileName){
+      System.out.println("Loading " + fileName);
+   }
+}
