f0570f3963803b7594c0cfbb817496a50a68f564
au-win <auzhouqingyu@163.com>
2022-05-23 22:16:18 +0800

    structural decorator example4 RedShapeDecorator and ShapeDecorator

---
+++ src/main/java/top/auzqy/design/pattern/structural/decorator/example4/RedShapeDecorator.java
@@ -0,0 +1,18 @@
+package top.auzqy.design.pattern.structural.decorator.example4;
+
+public class RedShapeDecorator extends ShapeDecorator {
+ 
+   public RedShapeDecorator(Shape decoratedShape) {
+      super(decoratedShape);     
+   }
+ 
+   @Override
+   public void draw() {
+      decoratedShape.draw();         
+      setRedBorder(decoratedShape);
+   }
+ 
+   private void setRedBorder(Shape decoratedShape){
+      System.out.println("Border Color: Red");
+   }
+}
---
+++ src/main/java/top/auzqy/design/pattern/structural/decorator/example4/ShapeDecorator.java
@@ -0,0 +1,13 @@
+package top.auzqy.design.pattern.structural.decorator.example4;
+
+public abstract class ShapeDecorator implements Shape {
+   protected Shape decoratedShape;
+ 
+   public ShapeDecorator(Shape decoratedShape){
+      this.decoratedShape = decoratedShape;
+   }
+ 
+   public void draw(){
+      decoratedShape.draw();
+   }  
+}
