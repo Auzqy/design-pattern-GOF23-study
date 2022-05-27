5bf946e92594abb609fa9af08f3ca3be7a7d63e4
au-win <auzhouqingyu@163.com>
2022-05-27 08:49:33 +0800

    behavioral mediator ChatRoom and User

---
+++ src/main/java/top/auzqy/design/pattern/behavioral/mediator/example3/ChatRoom.java
@@ -0,0 +1,10 @@
+package top.auzqy.design.pattern.behavioral.mediator.example3;
+
+import java.util.Date;
+ 
+public class ChatRoom {
+   public static void showMessage(User user, String message){
+      System.out.println(new Date().toString()
+         + " [" + user.getName() +"] : " + message);
+   }
+}
