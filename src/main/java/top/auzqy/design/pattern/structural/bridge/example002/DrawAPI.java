8543841156180f8b535418295eade6e601585b46
au-win <auzhouqingyu@163.com>
2022-05-31 17:40:56 +0800

    structural bridge example002 DrawAPI and GreenCircle and RedCircle

---
+++ src/main/java/top/auzqy/design/pattern/structural/bridge/example002/DrawAPI.java
@@ -0,0 +1,5 @@
+package top.auzqy.design.pattern.structural.bridge.example002;
+
+public interface DrawAPI {
+   public void drawCircle(int radius, int x, int y);
+}
---
+++ src/main/java/top/auzqy/design/pattern/structural/bridge/example002/GreenCircle.java
@@ -0,0 +1,9 @@
+package top.auzqy.design.pattern.structural.bridge.example002;
+
+public class GreenCircle implements DrawAPI {
+   @Override
+   public void drawCircle(int radius, int x, int y) {
+      System.out.println("Drawing Circle[ color: green, radius: "
+         + radius +", x: " +x+", "+ y +"]");
+   }
+}
