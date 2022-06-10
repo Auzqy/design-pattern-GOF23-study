899f3f321f37c0e10898a5940a69ae0f01deb9e6
au-win <auzhouqingyu@163.com>
2022-06-10 08:45:50 +0800

    templetemethod example3 TemplateMethodPattern

---
+++ src/main/java/top/auzqy/design/pattern/behavioral/templetemethod/example3/TemplateMethodPattern.java
@@ -0,0 +1,34 @@
+package top.auzqy.design.pattern.behavioral.templetemethod.example3;
+
+public class TemplateMethodPattern {
+    public static void main(String[] args) {
+        AbstractClass tm = new ConcreteClass();
+        tm.TemplateMethod();
+    }
+}
+//抽象类
+abstract class AbstractClass {
+    //模板方法
+    public void TemplateMethod() {
+        SpecificMethod();
+        abstractMethod1();
+        abstractMethod2();
+    }
+    //具体方法
+    public void SpecificMethod() {
+        System.out.println("抽象类中的具体方法被调用...");
+    }
+    //抽象方法1
+    public abstract void abstractMethod1();
+    //抽象方法2
+    public abstract void abstractMethod2();
+}
+//具体子类
+class ConcreteClass extends AbstractClass {
+    public void abstractMethod1() {
+        System.out.println("抽象方法1的实现被调用...");
+    }
+    public void abstractMethod2() {
+        System.out.println("抽象方法2的实现被调用...");
+    }
+}
