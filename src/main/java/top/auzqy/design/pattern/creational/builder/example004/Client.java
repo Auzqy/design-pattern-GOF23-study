1df40b2e3f7387b791a8ccd484a7b90f9ea97198
au-win <auzhouqingyu@163.com>
2022-07-07 16:33:26 +0800

    creational builder example004 Client

---
+++ src/main/java/top/auzqy/design/pattern/creational/builder/example004/Client.java
@@ -0,0 +1,10 @@
+package top.auzqy.design.pattern.creational.builder.example004;
+
+public class Client {
+    public static void main(String[] args) {
+        Builder builder = new ConcreteBuilder();
+        Director director = new Director(builder);
+        Product product = director.construct();
+        product.show();
+    }
+}
