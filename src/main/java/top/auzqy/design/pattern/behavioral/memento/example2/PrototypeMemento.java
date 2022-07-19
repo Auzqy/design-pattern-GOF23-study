bbe15ff0651efff2091bb3e4061b82d0a7668608
au-win <auzhouqingyu@163.com>
2022-07-19 08:47:47 +0800

    behavioral memento example2 PrototypeMemento

---
+++ src/main/java/top/auzqy/design/pattern/behavioral/memento/example2/PrototypeMemento.java
@@ -0,0 +1,49 @@
+package top.auzqy.design.pattern.behavioral.memento.example2;
+
+public class PrototypeMemento {
+    public static void main(String[] args) {
+        OriginatorPrototype or = new OriginatorPrototype();
+        PrototypeCaretaker cr = new PrototypeCaretaker();
+        or.setState("S0");
+        System.out.println("初始状态:" + or.getState());
+        cr.setMemento(or.createMemento()); //保存状态
+        or.setState("S1");
+        System.out.println("新的状态:" + or.getState());
+        or.restoreMemento(cr.getMemento()); //恢复状态
+        System.out.println("恢复状态:" + or.getState());
+    }
+}
+//发起人原型
+class OriginatorPrototype implements Cloneable {
+    private String state;
+    public void setState(String state) {
+        this.state = state;
+    }
+    public String getState() {
+        return state;
+    }
+    public OriginatorPrototype createMemento() {
+        return this.clone();
+    }
+    public void restoreMemento(OriginatorPrototype opt) {
+        this.setState(opt.getState());
+    }
+    public OriginatorPrototype clone() {
+        try {
+            return (OriginatorPrototype) super.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
+//原型管理者
+class PrototypeCaretaker {
+    private OriginatorPrototype opt;
+    public void setMemento(OriginatorPrototype opt) {
+        this.opt = opt;
+    }
+    public OriginatorPrototype getMemento() {
+        return opt;
+    }
+}
